1c462a5a538f4b610ef2fb09133f3bd63d452bc5
Sk00sha <40105125+Sk00sha@users.noreply.github.com>
2020-08-09 15:49:06 +0200

    Simple to-do app done,will do cookies

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var counter=0;
+var tasksdone=0;
 function getvaluefromform(){
     document.getElementById("nvm").addEventListener("click", function(event){
         event.preventDefault()
@@ -5,6 +7,8 @@ function getvaluefromform(){
     var headlight=document.getElementById("name");
     var task=document.getElementById("taskname");
     creatediv(headlight,task);
+    counter++;
+    console.log(counter);
 }
 function creatediv(head,task){
     var grid=document.getElementById("grid");
@@ -15,11 +19,22 @@ function creatediv(head,task){
     li.setAttribute("class","taskcard");
     li.addEventListener('click',function(){
         li.setAttribute('class','classdone');
+        tasksdone++;
+        console.log(taskcounter(counter,tasksdone));
     });
     p.textContent=task.value;
-    h.textContent=head.value;
+    h.textContent="Názov: "+head.value;
     li.appendChild(h);
     li.appendChild(p);
 uli.appendChild(li);
 grid.appendChild(uli);
+}
+function taskcounter(param1,param2) {
+    let percent=0;
+    if(param2>0){
+    percent= (param2/param1)*100;
+    document.querySelector(".html").style.width=percent+"%";
+    document.querySelector(".html").innerHTML=Math.ceil(percent)+"%";
+}
+return Math.ceil(percent);
 }
